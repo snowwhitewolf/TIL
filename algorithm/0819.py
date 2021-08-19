1cc9809212db07e330e05f474480582ec44059c0
snowwhitewolf <whdgus1029@naver.com>
2021-08-19 17:24:51 +0900

    0819

--- algorithm/0819.py
+++ algorithm/0819.py
@@ -1,9 +1,22 @@
 for t in range(int(input())):
     V, E = map(int, input().split())
-    lst = list([0 for _ in range(V)] for _ in range(V))
+    lst = list([0 for _ in range(V+1)] for _ in range(V+1))
     for e in range(E):
         a, b = map(int, input().split())
-        lst[a-1][b-1] = 1
-
+        lst[a][b] = 1
+    gg = 0
     S, G = map(int, input().split())
-    print(lst)
+    visited = list(0 for _ in range(V+1))
+    def dfs(now):
+        for next in range(1, V+1):
+            if lst[now][next] == 1 and visited[next] == 0:
+                if next == G:
+                    global gg
+                    gg = 1
+                    break
+                visited[next] = 1
+                dfs(next)
+        return
+
+    dfs(S)
+    print('#{} {}'.format(t+1,gg))
