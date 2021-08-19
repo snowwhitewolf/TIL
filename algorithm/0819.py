f04f040135ce37a4ed9b9f263369b63238bb2170
snowwhitewolf <whdgus1029@naver.com>
2021-08-19 15:59:49 +0900

    0819

--- algorithm/0819.py
+++ algorithm/0819.py
@@ -1,11 +1,9 @@
 for t in range(int(input())):
-    st = input()
-    lst = []
-    for i in st:
-        if i == '{' or i == '}' or i == '[' or i == ']' or i == '(' or i == ')':
-            lst.append(i)
+    V, E = map(int, input().split())
+    lst = list([0 for _ in range(V)] for _ in range(V))
+    for e in range(E):
+        a, b = map(int, input().split())
+        lst[a-1][b-1] = 1
+
+    S, G = map(int, input().split())
     print(lst)
-
-    def f(n):
-        if lst[n] == '{' :
-
