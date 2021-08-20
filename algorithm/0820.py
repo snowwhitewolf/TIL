ab6da263a9f875620ef2393335c9c70673820212
snowwhitewolf <whdgus1029@naver.com>
2021-08-20 17:55:14 +0900

    0820

--- algorithm/0820.py
+++ algorithm/0820.py
@@ -1,18 +1,7 @@
-for t in range(int(input())):
-    h , w = map(int,input().split())
-    d = []
-    for i in range(h):
-        d.append(list(map(int,input().split())))
-    ans = 0
-    total = []
-    for y in range(h):
-        for x in range(w):
-            total = []
-            for yy in range(h-y):
-                for xx in range(w-x):
-                    total.append(d[y+yy][x+xx])
-            print(total)
-            if sum(total) > ans:
-                ans = sum(total)
+lst = [5,3,2,7,9,8,6]
 
-    print(ans)
+for i in range(len(lst)):
+    for j in range(i,len(lst)):
+        if lst[i] > lst[j]:
+            lst[i], lst[j] = lst[j], lst[i]
+print(lst)
