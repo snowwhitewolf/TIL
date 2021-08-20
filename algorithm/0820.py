60c0ff34fe16a14a62e01becf7d10bbba7250821
snowwhitewolf <whdgus1029@naver.com>
2021-08-20 16:43:27 +0900

    0820

--- algorithm/0820.py
+++ algorithm/0820.py
@@ -1,10 +1,44 @@
-def rotate(lst):
-    backup = lst[-1]
-    for i in range(4, 0 - 1, -1):
-        lst[i + 1] = lst[i]
-    lst[0] = backup
+import time
 
-lst = [3,4,7,9,8,5]
-for i in range(3):
-    rotate(lst)
-    print(lst)
+def map_print(ny,nx): # @
+    for i in range(100): print(end = ' ')
+    print()
+    for y in range(7):
+        for x in range(10) :
+            if (y,x) == (ny,nx):
+                print("@",end='')
+            else :
+                print(MAP[y][x],end ='')
+        print()
+
+    time.sleep(0.7)
+
+def dfs(y,x):
+    if MAP[y][x] == '#': return
+    if visited[y][x] == 1 : return
+    visited[y][x] = 1
+    map_print(y,x)
+    dfs(y-1,x)
+    map_print(y, x)
+    dfs(y+1,x)
+    map_print(y, x)
+    dfs(y,x-1)
+    map_print(y, x)
+    dfs(y,x+1)
+    map_print(y, x)
+    return
+
+MAP = [
+    "##########",
+    "#_#___#__#",
+    "#_#_#_#_##",
+    "#_#_#_#__#",
+    "#_#_#_#_##",
+    "#___#____#",
+    "##########",
+]
+visited = [
+    [0 for _ in range(10)] for _ in range(7)
+]
+
+dfs(1,1)
