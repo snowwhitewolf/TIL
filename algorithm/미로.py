72275fbc91b86fee0b1de182e904cab15187d80c
snowwhitewolf <whdgus1029@naver.com>
2021-08-24 13:29:17 +0900

    0824

--- algorithm/미로.py
+++ algorithm/미로.py
@@ -1,6 +1,39 @@
+def dfs(y,x):
+    global cnt
+    if maze[y][x] == '1':
+        return
+    if visited[y][x] == 1:
+        return
+    if maze[y][x] == '3':
+        cnt = 1
+        return
+
+
+    visited[y][x] = 1
+    if 0<= x <= n-1 and 0<= y-1 <= n-1:
+        dfs(y-1,x) #위
+    if 0 <= x <= n - 1 and 0 <= y+1 <= n - 1:
+        dfs(y+1,x) #아래
+    if 0 <= x-1 <= n - 1 and 0 <= y <= n - 1:
+        dfs(y,x-1) #왼쪽
+    if 0 <= x+1 <= n - 1 and 0 <= y <= n - 1:
+        dfs(y,x+1) #오른쪽
+    return
+
+
 for t in range(int(input())):
     n = int(input())
     maze = []
     for i in range(n):
         maze.append(input())
     visited = [[0 for _ in range(n)] for _ in range(n)]
+    cnt = 0
+    s_y = 0
+    s_x = 0
+    for y in range(n):
+        for x in range(n):
+            if maze[y][x] == '2':
+                s_y = y
+                s_x = x
+    dfs(s_y,s_x)
+    print('#{} {}'.format(t+1,cnt))
--- algorithm/부분집합의합2.py
+++ algorithm/부분집합의합2.py
@@ -1,33 +1,19 @@
-def func(level):
-    global k
-    if level == n and sum(res) == k:
-        global cnt
-        cnt += 1
+def dfs(num, sum,cnt, path):
+    global ans
+    if cnt > n:
         return
-    for i in range(1,k-2):
-        if c[i-1] == 0:
-            res.append(i)
-            c[i-1] = 1
-            if sum(res) > k:
-                break
-            func(level+1)
-            c[i-1] = 0
-            res.pop()
-        else:
-            continue
+    if sum > k:
+        return
+    if num == 21:
+        if cnt == n and sum == k:
+            ans += 1
+        return
+    dfs(num+1, sum+num, cnt + 1,path+str(num))
+    dfs(num+1, sum,cnt,path)
     return
 
 for t in range(int(input())):
-    lst = [i for i in range(1, 21)]
-    c = [0 for _ in range(20)]
-    n,k = map(int,input().split())
-    res = []
-    cnt = 0
-    a = n
-    cc = 1
-    while a != 1:
-        cc *= a
-        a-= 1
-    func(0)
-    cnt //= cc
-    print('#{} {}'.format(t+1,cnt))
+    n, k = map(int, input().split())
+    ans = 0
+    dfs(1,0,0,"")
+    print('#{} {}'.format(t+1,ans))
