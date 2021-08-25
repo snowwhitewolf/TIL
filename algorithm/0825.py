508fcefa03a2c38a31a148057beb195dcd3d9d3b
snowwhitewolf <whdgus1029@naver.com>
2021-08-25 13:40:58 +0900

    0825

--- algorithm/0825.py
+++ algorithm/0825.py
@@ -1,15 +1,8 @@
-queue = [0,0,0,0,0,0,0,0,0,0]
+queue = [3,7,2,1,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 front = 0
-rear = 0
-queue[rear] = 3
-rear += 1
-queue[rear] = 7
-rear += 1
-queue[rear] = 5
-rear += 1
-ret = queue[front]
-print(ret)
-front += 1
-ret = queue[front]
-print(ret)
-front += 1
+rear = 5
+
+while front != rear:
+    ret = queue(front)
+    front += 1
+    print(ret)
