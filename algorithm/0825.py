248c44ed6fcc018f1978cb82f63d93a6da3b9589
snowwhitewolf <whdgus1029@naver.com>
2021-08-25 16:35:52 +0900

    0825

--- algorithm/0825.py
+++ algorithm/0825.py
@@ -1,8 +1,3 @@
-queue = [3,7,2,1,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-front = 0
-rear = 5
-
-while front != rear:
-    ret = queue(front)
-    front += 1
-    print(ret)
+for t in range(int(input())):
+    x,y = map(int,input().split())
+    result = a*b*h
