b06f8e8587e356f7b28b33252f87b7f492efe4ca
snowwhitewolf <whdgus1029@naver.com>
2021-08-25 16:55:17 +0900

    0825

--- algorithm/0825.py
+++ algorithm/0825.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 value = [0,1,1,3,2,4,5,2]
 lst = [[0] * 8 for _ in range(8)]
 lst[0][1] = 1
@@ -7,7 +9,12 @@ lst[1][4] = 1
 lst[1][5] = 1
 lst[1][6] = 1
 lst[3][7] = 1
-i = int(input())
+queue = deque()
 for j in range(8):
-    if lst[i][j] == 1:
-        print(value[j])
+    if lst[0][j] == 1:
+        queue.append(j)
+ret = queue.popleft()
+for j in range(8):
+    if lst[ret][j] == 1:
+        queue.append(j)
+print(queue)
