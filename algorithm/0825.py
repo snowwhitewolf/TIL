32956085cbd5fdacea30d45553303bf7ad2b99c8
snowwhitewolf <whdgus1029@naver.com>
2021-08-25 17:13:49 +0900

    0825

--- algorithm/0825.py
+++ algorithm/0825.py
@@ -10,11 +10,10 @@ lst[1][5] = 1
 lst[1][6] = 1
 lst[3][7] = 1
 queue = deque()
-for j in range(8):
-    if lst[0][j] == 1:
-        queue.append(j)
-ret = queue.popleft()
-for j in range(8):
-    if lst[ret][j] == 1:
-        queue.append(j)
-print(queue)
+queue.append(0)
+while queue:
+    now = queue.popleft()
+    print(now, end=' ')
+    for next in range(8):
+        if lst[now][next] == 1:
+            queue.append(next)
