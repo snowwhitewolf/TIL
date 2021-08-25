279b93b53a1017b2ad6d99adf980db04d901ce5f
snowwhitewolf <whdgus1029@naver.com>
2021-08-25 16:44:16 +0900

    0825

--- algorithm/0825.py
+++ algorithm/0825.py
@@ -1,3 +1,13 @@
-for t in range(int(input())):
-    x,y = map(int,input().split())
-    result = a*b*h
+value = [0,1,1,3,2,4,5,2]
+lst = [[0] * 8 for _ in range(8)]
+lst[0][1] = 1
+lst[0][2] = 1
+lst[0][3] = 1
+lst[1][4] = 1
+lst[1][5] = 1
+lst[1][6] = 1
+lst[3][7] = 1
+i = int(input())
+for j in range(8):
+    if lst[i][j] == 1:
+        print(value[j])
