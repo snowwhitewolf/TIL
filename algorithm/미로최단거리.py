11c9e7922b7c654bab6700bfdba275694c50c85d
snowwhitewolf <whdgus1029@naver.com>
2021-08-26 15:34:49 +0900

    0826

--- algorithm/미로최단거리.py
+++ algorithm/미로최단거리.py
@@ -8,20 +8,24 @@ for t in range(int(input())):
                 sty,stx = y, x
             elif maze[y][x] == 3:
                 resy,resx = y, x
-    print(stx,sty,resx,resy)
     dx = [-1,0,1,0]
     dy = [0, 1, 0, -1]
-    visited = [[0] * n for _ in range(n)]
-    queue = deque()
-    queue.append((sty,stx))
-    while queue:
-        y,x = queue.popleft()
-        if x == resx and y == resy:
-            break
-        for i in range(4):
-            nextx = x + dx[i]
-            nexty = y + dy[i]
-            if 0 <= nextx < n and 0 <= nexty < n and maze[nexty][nextx] == 0 and visited[nexty][nextx]==0:
-                queue.append((nexty,nextx))
-                visited[nexty][nextx] = visited[sty][stx] + 1
-    print(visited)
+    def bfs(sty1,stx1):
+        visited = [[0] * n for _ in range(n)]
+        queue = deque()
+        queue.append((sty1, stx1))
+        while queue:
+            y,x = queue.popleft()
+            if x == resx and y == resy:
+                break
+            for i in range(4):
+                nextx = x + dx[i]
+                nexty = y + dy[i]
+                if 0 <= nextx < n and 0 <= nexty < n:
+                    if maze[nexty][nextx] == 0 and visited[nexty][nextx]==0:
+                        queue.append((nexty, nextx))
+                        visited[nexty][nextx] = visited[y][x] + 1
+                    if maze[nexty][nextx] == 3:
+                        return visited[y][x]
+        return 0
+    print('#{} {}'.format(t+1,bfs(sty,stx)))
