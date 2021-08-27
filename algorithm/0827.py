5563f730de79b8bca8807778936adb3a420e481a
snowwhitewolf <whdgus1029@naver.com>
2021-08-27 14:02:40 +0900

    0827

--- algorithm/0827.py
+++ algorithm/0827.py
@@ -3,28 +3,18 @@ def dfs(now) :
         return
     preorder.append(now)
     dfs(left[now])
+    inorder.append(now)
     dfs(right[now])
     postorder.append(now)
     return
 
-left = [0 for _ in range(11)]
-right = [0 for _ in range(11)]
-
-root = 1
-left[1] = 3
-right[1] = 5
-left[3] = 2
-right[3] = 6
-left[5] = 7
-right[5] = 8
-left[7] = 9
-left[9] = 10
-right[8] = 4
-
+lst = [0 for _ in range(11)]
+lst[2*1] = 3
+lst[2*1+1] = 5
 preorder = []
 postorder = []
 inorder  = []
 dfs(root)
-print(preorder)
-print(postorder)
-print(inorder)
+print(*preorder)
+print(*postorder)
+print(*inorder)
