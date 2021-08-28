f8006b691a8598eb4227f7e784b4f5375ac070e6
snowwhitewolf <whdgus1029@naver.com>
2021-08-29 08:00:06 +0900

    0828

---
+++ algorithm/스도쿠 검증.py
@@ -0,0 +1,7 @@
+#9*9 배열이 주이짐
+#가로 세로 네모를 따져서 하나라도 안되면 바로 리턴해서 끝냄
+#가로 세로 네모를 각각의 함수로 만들어서 검증
+
+for t in range(int(input())):
+    lst = [list(map(int,input().split())) for _ in range(9)]
+    print(lst)
