0e5b7563650752741933c28a54fe20c2b7864d61
snowwhitewolf <whdgus1029@naver.com>
2021-09-23 13:14:53 +0900

    0923

--- algorithm/0923.py
+++ algorithm/0923.py
@@ -1,11 +1,12 @@
-lst = ['X','A','B','C','D','E','X','F','G','H']
+lst = [0,1,2,3,4,5,6,7,8]
 def dfs(now):
     if now >= len(lst) : return
-    if lst[now] == 'X': return
+    if lst[now] == 0: return
 
     dfs(now*2) # left subtree
     print(lst[now], end=' ')
     dfs(now*2+1) # right subtree
+
     return
 
 dfs(1)
