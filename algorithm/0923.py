8eb174c7d6474acccdd7c3b1b927a575b80060cb
snowwhitewolf <whdgus1029@naver.com>
2021-09-24 12:41:44 +0900

    0923

--- algorithm/0923.py
+++ algorithm/0923.py
@@ -1,12 +1,35 @@
-lst = [0,1,2,3,4,5,6,7,8]
-def dfs(now):
-    if now >= len(lst) : return
-    if lst[now] == 0: return
+N,K = map(int,input().split())
+A = list(map(int,input().split()))
+robot = [0 for _ in range(2*N)]
+cnt = 0
+while True:
+    # 1번째 단계
+    cnt += 1
+    next = A[-1]
+    del A[-1]
+    A.insert(0,A.pop())
+    next = robot[-1]
+    del robot[-1]
+    robot.insert(0, next)
+    if robot[N-1]:
+        robot[N-1]-=1
 
-    dfs(now*2) # left subtree
-    print(lst[now], end=' ')
-    dfs(now*2+1) # right subtree
+    # 2번째 단계
+    for i in range(N-2,-1,-1):
+        if robot[i] and A[i+1] and robot[i+1]==0:
+            robot[i]-=1
+            robot[i+1]+=1
+            A[i+1] -= 1
+    if robot[N - 1]:
+        robot[N - 1] = 0
 
-    return
+    # 3번째 단계
+    if A[0] and robot[0]==0:
+        robot[0] = 1
+        A[0] -= 1
 
-dfs(1)
+    # 4번째 단계
+    if A.count(0) >= K:
+        break
+
+print(cnt)
