43b1a640bb166b00f006806e77b06d007ad6eec6
snowwhitewolf <whdgus1029@naver.com>
2021-09-24 15:56:17 +0900

    0924

---
+++ algorithm/물놀이를가자.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+dr = [1,-1,0,0]
+dc = [0,0,1,-1]
+for t in range(int(input())):
+    N,M = map(int,input().split())
+    arr = [input() for _ in range(N)]
+    dist = [[987654321] * M for _ in range(N)]   
+
+    Q = deque()
+
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] == 'W':
+                Q.append((i,j))
+                dist[i][j] = 0
+    while Q:
+        r,c = Q.popleft()
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+
+            if nr<0 or nr>=N or nc<0 or nc>=M:
+                continue
+            if arr[nr][nc] == 'L' and dist[nr][nc] == 987654321:
+                dist[nr][nc] = dist[r][c]+1
+                Q.append((nr,nc))
+    ans = 0
+
+    for i in dist:
+        for j in i:
+            ans+=j
+    print('#{} {}'.format(t+1,ans))
