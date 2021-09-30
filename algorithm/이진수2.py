5573245e4ed71847241e239aff97030282e2e3f4
snowwhitewolf <whdgus1029@naver.com>
2021-09-30 12:57:14 +0900

    0929

---
+++ algorithm/이진수2.py
@@ -0,0 +1,17 @@
+for t in range(int(input())):
+    n = float(input())
+    res = ''
+    while len(res)<=13:
+        n *= 2
+        if n > 1:
+            n -= 1
+            res += '1'
+        elif n == 1.0:
+            res += '1'
+            break
+        else:
+            res += '0'
+    if len(res)<= 12:
+        print('#{} {}'.format(t+1,res))
+    else:
+        print('#{} overflow'.format(t+1))
