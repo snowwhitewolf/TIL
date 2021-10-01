d802f1bc02e0c7dc517fa62a72e5a94555c2d56d
snowwhitewolf <whdgus1029@naver.com>
2021-10-01 17:23:05 +0900

    0929

---
+++ algorithm/N과M(4).py
@@ -0,0 +1,13 @@
+lst = [1,2,3,4,5]
+res= set()
+def f(level,path):
+    if level == len(lst):
+        return
+    res.add(path)
+    f(level + 1 , path + f'{lst[level]}')
+    res.add(path)
+    f(level + 1, path)
+
+
+f(0,'')
+print(sorted(res))
