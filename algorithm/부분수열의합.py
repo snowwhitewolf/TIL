5b64b9b87ba4cb600e0cd906989f16902b8cd1b1
snowwhitewolf <whdgus1029@naver.com>
2021-10-05 10:24:38 +0900

    1005

--- algorithm/부분수열의합.py
+++ algorithm/부분수열의합.py
@@ -1,2 +1,20 @@
 import sys
 sys.stdin= open('input.txt', "r")
+
+N,S = map(int,input().split())
+lst = list(map(int,input().split()))
+res = 0
+def f(level, s):
+    global res
+    if level ==N:
+        if s == S:
+            res += 1
+        return
+    f(level + 1, s)
+    f(level + 1, s + lst[level])
+
+f(0,0)
+if S == 0:
+    res -= 1
+print(res)
+
