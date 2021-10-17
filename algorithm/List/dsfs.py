a0b8a8bfa25fa06f813eae2de46b2665c4964acf
snowwhitewolf <whdgus1029@naver.com>
2021-10-17 22:30:10 +0900

    1017

--- algorithm/List/dsfs.py
+++
@@ -1,7 +0,0 @@
-import math
-for t in range(int(input())):
-    N = int(input())
-    result = -1
-    if math.isclose((N**(1/3))+0.000000000000001,((N**(1/3))+0.000000000000001)//1):
-        result = int((N**(1/3))+0.000000000000001)
-    print('#{} {}'.format(t + 1, result))
