7c6c1b69132b12ff242f5da0dd737fe5c99d6af2
snowwhitewolf <whdgus1029@naver.com>
2021-10-25 18:32:46 +0900

    1025

--- algorithm/1025/2751.py
+++ algorithm/1025/2751.py
@@ -1,9 +1,9 @@
 import sys
 sys.stdin = open('input.txt', 'r')
-
+import sys
 res = [0]*10001
 for t in range(int(input())):
-    n = int(input())
+    n = int(sys.stdin.readline())
     res[n]+= 1
 for i in range(1,10001):
     if res[i]:
