1169f27d0c76d69fdbb011f0c0eb622b6a5e9299
snowwhitewolf <whdgus1029@naver.com>
2021-10-25 17:48:33 +0900

    1025

--- algorithm/1019/상어초등학교.py
+++ algorithm/1019/상어초등학교.py
@@ -1,5 +1,5 @@
 import sys
-sys.stdin = open('../1023/input.txt', 'r')
+sys.stdin = open('../1025/input.txt', 'r')
 
 def first(man):
     total = []
--- algorithm/1023/1023.py
+++ algorithm/1023/1023.py
@@ -1,5 +1,5 @@
 import sys
-sys.stdin = open('input.txt','r')
+sys.stdin = open('input.txt', 'r')
 
 a,b,c, = map(int,input().split())
 
