ec6fb36fe39d746bb7c65d5946ddc50600881638
snowwhitewolf <whdgus1029@naver.com>
2021-10-26 18:36:30 +0900

    1026

--- algorithm/1026/1026.py
+++ algorithm/1026/1026.py
@@ -1,11 +1,8 @@
 n = int(input())
-if n >= 90:
-    print('A')
-elif n >= 80:
-    print('B')
-elif n >= 70:
-    print('C')
-elif n >= 60:
-    print('D')
-else:
-    print('F')
+lst = [0] * 1001
+for i in range(1,100):
+    lst[i] = 1
+for i in range(100,1000):
+    if int(str(i)[0]) - int(str(i)[1]) == int(str(i)[1]) - int(str(i)[2]):
+        lst[i] = 1
+print(lst[1:n+1].count(1))
