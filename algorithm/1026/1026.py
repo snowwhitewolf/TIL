02f660d45359e241efcdd73cdcfed1e5c080e684
snowwhitewolf <whdgus1029@naver.com>
2021-10-26 18:52:01 +0900

    1026

--- algorithm/1026/1026.py
+++ algorithm/1026/1026.py
@@ -1,8 +1,14 @@
 n = int(input())
-lst = [0] * 1001
-for i in range(1,100):
-    lst[i] = 1
-for i in range(100,1000):
-    if int(str(i)[0]) - int(str(i)[1]) == int(str(i)[1]) - int(str(i)[2]):
-        lst[i] = 1
-print(lst[1:n+1].count(1))
+
+dp = [0 for _ in range(n + 1)]
+
+for i in range(2, n + 1):
+    dp[i] = dp[i - 1] + 1
+
+    if i % 2 == 0 and dp[i] > dp[i // 2] + 1:
+        dp[i] = dp[i // 2] + 1
+
+    if i % 3 == 0 and dp[i] > dp[i // 3] + 1:
+        dp[i] = dp[i // 3] + 1
+
+print(dp[n])
