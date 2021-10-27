e3d9f8d485900a04f28a9a709d189f80e929c68b
snowwhitewolf <whdgus1029@naver.com>
2021-10-27 16:30:23 +0900

    1027

--- algorithm/1027/1027.py
+++ algorithm/1027/1027.py
@@ -1,1 +1,12 @@
-N = int(input())
+N = input()
+if len(N)== 1:
+    N = '0' + N
+N2 = N
+cnt = 0
+while True:
+    num = int(N2[0]) + int(N2[1])
+    N2 = N[1] + str(num)
+    cnt += 1
+    if  N == N2:
+        print(cnt)
+        break
