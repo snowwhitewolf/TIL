ee034872dfda57a0ec39f06c2f7085f75cece086
snowwhitewolf <whdgus1029@naver.com>
2021-11-12 17:40:20 +0900

    1111

--- algorithm/1111/1111.py
+++ algorithm/1111/1111.py
@@ -11,23 +11,4 @@ def f1(lst):
         if lst[i] == m:
             lst[i] += 1
 
-def f2(lst):
-    global MAP
-    MAP2 = []
-    MAP2.append(lst[0])
-    MAP = MAP[1:]
-    MAP2.append(MAP)
-    MAP = MAP2
-
-def f4(lst):
-    for i in range(0,N//2):
-
-while True:
-    cnt += 1
-    f1(MAP)
-    f2(MAP)
-    print(MAP)
-    if max(MAP)-min(MAP) <= K:
-        break
-
 print(cnt)
