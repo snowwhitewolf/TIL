8bd59623eefa86ccf5120ccc2ce40c672e5e8ba6
snowwhitewolf <whdgus1029@naver.com>
2021-12-05 00:14:50 +0900

    1205

--- algorithm/1204/16235.py
+++ algorithm/1204/16235.py
@@ -2,4 +2,9 @@ import sys
 sys.stdin = open('input.txt', 'r')
 
 N,M,K = map(int,input().split())
-MAP = [list(map(int,input().split())) for _ in range(N)]
+A = [list(map(int,input().split())) for _ in range(N)]
+MAP = [[0]*N for _ in range(N)]
+for _ in range(M):
+    x,y,age = map(int,input().split())
+    MAP[y-1][x-1] = age
+print(MAP)
