4610a421ce2dae51d206009317d189cca4b96b91
snowwhitewolf <whdgus1029@naver.com>
2021-12-09 12:27:05 +0900

    1209

--- algorithm/1209/1978.py
+++ algorithm/1209/1978.py
@@ -1,8 +1,20 @@
 N = int(input())
 lst = list(map(int,input().split()))
 res = [1] * 1001
-sosu = [2,3,5,7,11,13,17,19,23,29,31,37]
+res[1] = 0
+total = 0
+sosu = [2,3,5,7,11,13,17,19,23,29]
 for i in sosu:
-    for j in range(501):
-        while
-for i in range(2,1001):
+    j = 2
+    while True:
+        if i*j >= 1001:
+            break
+        if res[i * j]:
+            res[i*j] = 0
+        j += 1
+
+
+for i in lst:
+    if res[i]:
+        total += 1
+print(total)
