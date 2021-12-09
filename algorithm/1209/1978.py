c86ea2a9d810bdea24d47d40af73b4ec9f9c4028
snowwhitewolf <whdgus1029@naver.com>
2021-12-09 12:12:32 +0900

    1209

---
+++ algorithm/1209/1978.py
@@ -0,0 +1,8 @@
+N = int(input())
+lst = list(map(int,input().split()))
+res = [1] * 1001
+sosu = [2,3,5,7,11,13,17,19,23,29,31,37]
+for i in sosu:
+    for j in range(501):
+        while
+for i in range(2,1001):
