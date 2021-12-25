c40024e51c0c5df92b6888621ccba5235ddb2625
snowwhitewolf <whdgus1029@naver.com>
2021-12-25 23:47:09 +0900

    1225

--- algorithm/1316.py
+++ algorithm/1316.py
@@ -1,4 +1,14 @@
+cnt = 0
 for t in range(int(input())):
+    a = 1
     n = input()
-
-
+    lst = []
+    for i in range(len(n)):
+        if n[i] not in lst:
+            lst.append(n[i])
+        elif n[i] != n[i-1]:
+            a = 0
+            break
+    if a:
+        cnt += 1
+print(cnt)
